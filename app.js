cc0bafe58d98bd17c89df6eaec2f5dc5688fa4f9
Matt Harris <mateodelay@gmail.com>
2016-03-22 18:28:17 -0600

    Fixed app.js problems.

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-//Module for weather app
-var weatherApp = angular.module('weatherApp',['ngRoute','ngResource',]);
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
-//Routes for single page application
-weatherApp.config(function($routeProvider){
+// ROUTES
+weatherApp.config(function ($routeProvider) {
 
 	$routeProvider
 
-	.when('/',{
-		templateUrl: 'pages/home.htm',
-		controller: 'homeController'
-	})
+		.when('/', {
+			templateUrl: 'pages/home.htm',
+			controller: 'homeController'
+		})
 
-	.when('/forecast',{
-		templateUrl:'/pages/forecast.htm',
-		controller:'forecastController'
-	})
+		.when('/forecast', {
+			templateUrl: 'pages/forecast.htm',
+			controller: 'forecastController'
+		})
 
 });
 
-//Controllers
-weatherApp.controller('homeController', ['$scope', function($scope){
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', function($scope) {
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope){
+weatherApp.controller('forecastController', ['$scope', function($scope) {
 
 }]);
